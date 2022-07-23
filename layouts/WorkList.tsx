3560797aa8b0d3ea505d92d127e8b2514b374c09
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2022-07-23 19:42:30 -0400

    style: adjusts some text content and typography (#4)

--- layouts/WorkList.tsx
+++ layouts/WorkList.tsx
@@ -5,7 +5,7 @@ export default function WorkList({ works }: { works: Work[] }) {
   return (
     <section className="mx-auto w-screen max-w-3xl">
       <div>
-        <h1 className="mb-3 text-6xl">Work</h1>
+        <h1 className="mb-3 text-6xl font-extralight italic">Work</h1>
         <p>
           Since I primarily work in the field of technical curriculum development, I find it important (and exciting) to
           stay up to date with new technologies, trends, and techniques in software development. Below are some of the
